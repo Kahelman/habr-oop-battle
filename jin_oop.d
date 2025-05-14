c80547e1c09bd1bfb0ed4628c3b89c4f02fcf244
nin-jin <sairi-na-tenshi@ya.ru>
2025-05-14 23:42:59 +0300

    Update jin_oop.d

--- jin_oop.d
+++ jin_oop.d
@@ -42,7 +42,7 @@ class Circle: Shape {
     
     uint radius;
     
-    this( int x, int y, int radius ) {
+    this( int x, int y, uint radius ) {
         this.anchor = Coord( x, y );
         this.radius = radius;
     }
@@ -53,7 +53,22 @@ class Circle: Shape {
     
 }
 
-class Rectangle: Shape {
+class Square: Shape {
+    
+    uint size;
+    
+    this( int x, int y, uint size ) {
+        this.anchor = Coord( x, y );
+        this.size = size;
+    }
+    
+    override void draw( Canvas canvas ) const {
+        canvas.paint( "	Square( anchor:", this.anchor, ", size:", this.size, " )" );
+    }
+    
+}
+
+class Line: Shape {
     
     Coord target;
     
@@ -63,11 +78,76 @@ class Rectangle: Shape {
     }
     
     override void draw( Canvas canvas ) const {
-        canvas.paint( "	Rectangle( anchor:", this.anchor, ", target:", this.target, " )" );
+        canvas.paint( "	Line( anchor:", this.anchor, ", target:", this.target, " )" );
     }
     
 }
 
+class Rect: Shape {
+    
+    Coord target;
+    
+    this( int ax, int ay, int tx, int ty ) {
+        this.anchor = Coord( ax, ay );
+        this.target = Coord( tx, ty );
+    }
+    
+    override void draw( Canvas canvas ) const {
+        canvas.paint( "	Rect( anchor:", this.anchor, ", target:", this.target, " )" );
+    }
+    
+}
+
+class Oval: Shape {
+    
+    Coord target;
+    uint radius;
+    
+    this( int ax, int ay, int tx, int ty, uint radius ) {
+        this.anchor = Coord( ax, ay );
+        this.target = Coord( tx, ty );
+        this.radius = radius;
+    }
+    
+    override void draw( Canvas canvas ) const {
+        canvas.paint( "	Oval( anchor:", this.anchor, ", target:", this.target, ", radius:", this.radius, " )" );
+    }
+    
+}
+
+class Rhomb: Shape {
+    
+    Coord target;
+    uint width;
+    
+    this( int ax, int ay, int tx, int ty, uint width ) {
+        this.anchor = Coord( ax, ay );
+        this.target = Coord( tx, ty );
+        this.width = width;
+    }
+    
+    override void draw( Canvas canvas ) const {
+        canvas.paint( "	Rhomb( anchor:", this.anchor, ", target:", this.target, ", width:", this.width, " )" );
+    }
+    
+}
+
+class Trigon: Shape {
+    
+    Coord second;
+    Coord third;
+    
+    this( int ax, int ay, int sx, int sy, int tx, int ty ) {
+        this.anchor = Coord( ax, ay );
+        this.second = Coord( sx, sy );
+        this.third = Coord( tx, ty );
+    }
+    
+    override void draw( Canvas canvas ) const {
+        canvas.paint( "	Trigon( anchor:", this.anchor, ", second:", this.second, ", third:", this.third, " )" );
+    }
+    
+}
 
 class Scene: Shape {
     
@@ -88,7 +168,12 @@ void main() {
     
     scene.shapes ~= new Point( 1, 1 );
     scene.shapes ~= new Circle( 5, 5, 3 ),
-    scene.shapes ~= new Rectangle( 0, 0, 6, 3 ),
+    scene.shapes ~= new Square( 7, 7, 2 ),
+    scene.shapes ~= new Line( 1, 2, 3, 4 );
+    scene.shapes ~= new Rect( 0, 0, 6, 3 ),
+    scene.shapes ~= new Oval( 9, 7, 6, 5, 3 ),
+    scene.shapes ~= new Rhomb( 6, 7, 3, 5, 2 ),
+    scene.shapes ~= new Trigon( 3, 7, 8, 9, 6, 1 ),
     scene.draw( stdout );
 
     scene.shapes[0].anchor = Coord( 2, 2 );
