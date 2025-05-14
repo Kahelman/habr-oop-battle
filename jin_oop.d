f149e8c76d259f300441a9dfb5cfb28e4e3781d8
nin-jin <sairi-na-tenshi@ya.ru>
2025-05-14 16:16:53 +0300

    Update jin_oop.d

--- jin_oop.d
+++ jin_oop.d
@@ -4,68 +4,90 @@
 //
 import std;
 
-interface Shape {
-    void draw( File output );
+immutable interface Shape {
+    void draw( File output ) const;
 }
 
 immutable struct Coord {
+    
     int x;
     int y;
+    
     string toString() {
         return "[ x:" ~ this.x.to!string ~ ", y:" ~ this.y.to!string ~ " ]";
     }
+    
 }
 
-class Point: Shape {
+immutable class Point: Shape {
+    
     Coord pos;
+    
     this( int x, int y ) {
         this.pos = Coord( x, y );
     }
-    void draw( File output ) {
+    
+    void draw( File output ) const {
         output.writeln( "Drawing Point( pos:", this.pos, " )" );
     }
+    
 }
 
-class Circle: Shape {
+immutable class Circle: Shape {
+    
     Coord center;
     int radius;
+    
     this( int x, int y, int radius ) {
         this.center = Coord( x, y );
         this.radius = radius;
     }
-    void draw( File output ) {
+    
+    void draw( File output ) const {
         writeln( "Drawing Circle( center:", this.center, ", radius:", this.radius, " )" );
     }
+    
 }
 
-class Rectangle: Shape {
+immutable class Rectangle: Shape {
+    
     Coord from;
     Coord to;
+    
     this( int fx, int fy, int tx, int ty ) {
         this.from = Coord( fx, fy );
         this.to = Coord( tx, ty );
     }
-    void draw( File output ) {
+    
+    void draw( File output ) const {
         writeln( "Drawing Rectangle( from:", this.from, ", to:", this.to, " )" );
     }
+    
 }
 
 
-class Scene {
+immutable class Scene {
+    
     Shape[] shapes;
-    this( Shape[] shapes ... ) {
+    
+    this( immutable(immutable(Shape)[]) shapes ... ) {
         this.shapes = shapes;
     }
+    
     void render( File output ) {
         foreach( shape; shapes ) shape.draw( output );
     }
+    
 }
 
 void main() {
+    
     auto scene = new Scene(
     	new Point( 1, 1 ),
     	new Circle( 5, 5, 3 ),
     	new Rectangle( 0, 0, 6, 3 ),
     );
+    
     scene.render( stdout );
+    
 }
